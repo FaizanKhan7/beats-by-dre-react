58ffc8ba85f26dcb18d2b130b45355d05c7a00bf
akif00 <akifmohammed0015@gmail.com>
2020-07-25 14:17:57 +0530

    Style modification for form

--- src/shared/components/Form/Form.js
+++ src/shared/components/Form/Form.js
@@ -3,27 +3,29 @@ import styles from "./form.module.css";
 
 function Form() {
   return (
-    <div className={styles.form}>
-      <h1 className={styles.header}> stay connected </h1>
-      <h4 className={styles.news}> beates newsletter </h4>
-      <p className={styles.statement}>
-        sign up now to hear about new stories, product drops, collaborations,
-        and all things beats - straight from your box
-      </p>
-      <input
-        type="email"
-        placeholder="Enter Your Email"
-        className={styles.email}
-      />
-      <p>
-        <input type="checkbox" />
-        <span>*</span> I Am 16 Years Of Age Or Older And Agree With The Terms &
-        Conditions And Privacy Policy
-      </p>
-      <p>
-        <input type="checkbox" />
-        <span>*</span> I Would Like To Receive Marketing Emails From Beats
-      </p>
+    <div className={styles.formbg}>
+      <div className={styles.form}>
+        <h1 className={styles.header}> stay connected </h1>
+        <h3 className={styles.news}> beates newsletter </h3>
+        <p className={styles.statement}>
+          sign up now to hear about new stories, product drops, collaborations,
+          and all things beats - straight from your box
+        </p>
+        <input
+          type="email"
+          placeholder="Enter Your Email"
+          className={styles.email}
+        />
+        <p className={styles.terms}>
+          <input type="checkbox" className={styles.check} />
+          <span>*</span> I Am 16 Years Of Age Or Older And Agree With The Terms
+          & Conditions And Privacy Policy
+        </p>
+        <p className={styles.terms}>
+          <input type="checkbox" className={styles.check} />
+          <span>*</span> I Would Like To Receive Marketing Emails From Beats
+        </p>
+      </div>
     </div>
   );
 }
