c19343e243472f0c5c57f257d7443d04ab522cf1
Mohamed Faizan khan <53424289+FaizanKhan7@users.noreply.github.com>
2020-09-01 20:06:10 +0530

    Added cards & routes in stories page and fixed small bugs in ui (#6)

--- src/Stories.js
+++ src/Stories.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Link, Switch, Route } from "react-router-dom";
 import styles from "./stories.module.css";
 import Tile from "./shared/components/Tile/Tile";
+
 import ProductRoutes from "./product/ProductRoutes/Product";
 import SupportRoutes from "./support/SupportRoutes/Support";
 import SearchRoutes from "./search/SearchRoutes/Search";
 import MenuBarRoutes from "./menubar/MenuBarRoutes/MenuBar";
 import Hamburger from "./shared/components/Hamburger/Hamburger";
-
+import StoriesCard from "./shared/components/StoriesCard/StoriesCard";
+import readStoriesRoutes from "./readStories/readStoriesRoutes/readStories";
 class Stories extends React.Component {
   state = {
     visible: false,
@@ -56,7 +58,76 @@ class Stories extends React.Component {
             <i className={"fas fa-search"}></i>
           </Link>
         </header>
-        <p>This is stories page</p>
+        <section className={styles.floatStories}>
+          <Link to="/readStories">
+            <StoriesCard
+              img={require("./shared/assets/stories-assets/powerbeats-yuki.og.jpg")}
+              headline={"The queen of all trades puts powerbeats to the test"}
+            />
+          </Link>
+          <Link to="/readStories">
+            <StoriesCard
+              img={require("./shared/assets/stories-assets/card-belli-ellish.png")}
+              headline={
+                "Inside the creative process with billie eilish and solo pro"
+              }
+            />
+          </Link>
+        </section>
+        <section className={styles.floatStories}>
+          <Link to="/readStories">
+            <StoriesCard
+              img={require("./shared/assets/stories-assets/brodinski.jpg")}
+              headline={"Brodinski: French electronics meets sourther trap"}
+            />
+          </Link>
+          <Link to="/readStories">
+            <StoriesCard
+              img={require("./shared/assets/stories-assets/donwood.jpg")}
+              headline={"Radiohead artist details his beats customs read"}
+            />
+          </Link>
+          <Link to="/readStories">
+            <StoriesCard
+              img={require("./shared/assets/stories-assets/sacai.jpg")}
+              headline={
+                "Beats / sacai: a celebration of creativity and unique style"
+              }
+            />
+          </Link>
+        </section>
+        <section className={styles.floatStories}>
+          <Link to="/readStories">
+            <StoriesCard
+              img={require("./shared/assets/stories-assets/don-collar.png")}
+              headline={
+                "How don c's collab pays homage to the nba, chicago and music"
+              }
+            />
+          </Link>
+          <Link to="/readStories">
+            <StoriesCard
+              img={require("./shared/assets/stories-assets/listeningmodes.jpg")}
+              headline={"control your sound with listening modes"}
+            />
+          </Link>
+        </section>
+        <section className={styles.floatStories}>
+          <Link to="/readStories">
+            <StoriesCard
+              img={require("./shared/assets/stories-assets/zerodark.jpg")}
+              headline={"a 24-hor relay chasing the icelanding sun"}
+            />
+          </Link>
+          <Link to="/readStories">
+            <StoriesCard
+              img={require("./shared/assets/stories-assets/pharrell.jpg")}
+              headline={
+                "the process and inspiration behind pharrell's more matte collection"
+              }
+            />
+          </Link>
+        </section>
       </div>
     );
   }
@@ -70,6 +141,7 @@ export default function Router() {
       <Route path="/support" component={SupportRoutes} />
       <Route path="/search" component={SearchRoutes} />
       <Route path="/menubar" component={MenuBarRoutes} />
+      <Route path="/readStories" component={readStoriesRoutes} />
     </Switch>
   );
 }
